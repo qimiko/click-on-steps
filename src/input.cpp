ce319523cceae8a703f9a14f66bc76e98234e9c5
Chloe <25387744+qimiko@users.noreply.github.com>
2024-05-15 17:09:05 -0700

    avoid memory allocations in input

--- src/input.cpp
+++ src/input.cpp
@@ -18,11 +18,11 @@ bool ExtendedCCKeyboardDispatcher::dispatchKeyboardMSG(cocos2d::enumKeyCodes key
 	geode::log::debug("dispatch keyboard event with time {}", g_lastTimestamp);
 #endif
 
-	g_currentEventInfo = new ExtendedCCEvent(g_lastTimestamp);
+	auto event = ExtendedCCEvent(g_lastTimestamp);
+	g_currentEventInfo = &event;
 
 	auto r = CCKeyboardDispatcher::dispatchKeyboardMSG(key, isKeyDown, isKeyRepeat);
 
-	delete g_currentEventInfo;
 	g_currentEventInfo = nullptr;
 	g_lastTimestamp = 0ull;
 
@@ -36,7 +36,7 @@ void ExtendedCCTouchDispatcher::setTimestamp(std::uint64_t timestamp) {
 }
 
 void ExtendedCCTouchDispatcher::touches(cocos2d::CCSet* touches, cocos2d::CCEvent* event, unsigned int index) {
-	auto extEvent = new ExtendedCCEvent(g_lastTimestamp);
-	CCTouchDispatcher::touches(touches, extEvent, index);
+	auto extEvent = ExtendedCCEvent(g_lastTimestamp);
+	CCTouchDispatcher::touches(touches, &extEvent, index);
 	g_lastTimestamp = 0ull;
 }
